82b05d596417aed7faf612f3e080e9f615e96fc5
Siti Ayuni <siti.ayuni21@students.unila.ac.id>
2024-01-18 09:35:58 +0700

    Memperbaiki landing page

---
+++ app/Views/nonup2d/tim.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>UP2D Lampung</title>
+    <link rel="shortcut icon" href="<?= base_url("assets/images/logo_pln.png") ?>">
+
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Bootstarp -->
+    <link href="<?= base_url("https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css") ?>" rel="stylesheet">
+
+    <!-- Favicons -->
+    <link href="<?= base_url("assets/img/favicon.png") ?>" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="<?= base_url("assets/vendor/aos/aos.css") ?>" rel="stylesheet">
+    <link href="<?= base_url("assets/vendor/bootstrap/css/bootstrap.min.css") ?>" rel="stylesheet">
+    <link href="<?= base_url("assets/vendor/bootstrap-icons/bootstrap-icons.css") ?>" rel="stylesheet">
+    <link href="<?= base_url("assets/vendor/boxicons/css/boxicons.min.css") ?>" rel="stylesheet">
+    <link href="<?= base_url("assets/vendor/glightbox/css/glightbox.min.css") ?>" rel="stylesheet">
+    <link href="<?= base_url("assets/vendor/remixicon/remixicon.css") ?>" rel="stylesheet">
+    <link href="<?= base_url("assets/vendor/swiper/swiper-bundle.min.css") ?>" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="<?= base_url("assets/css/styles.css") ?>" rel="stylesheet">
+
+    <!-- =======================================================
+  * Template Name: Arsha
+  * Updated: Sep 18 2023 with Bootstrap v5.3.2
+  * Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top ">
+        <div class="container d-flex align-items-center">
+
+            <h1 class="logo me-auto">
+                <a href="landingpage.php">
+                    <img src="<?= base_url("assets/images/logo_pln.png") ?>" alt="UP2D Logo" class="img-fluid">
+                    UP2D LAMPUNG
+                </a>
+            </h1>
+
+            <!-- Uncomment below if you prefer to use an image logo -->
+            <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li><a class="nav-link scrollto" href="<?= base_url("/UP2D Lampung") ?>">Beranda</a></li>
+                    <li><a class="nav-link scrollto" href="#about">Cari</a></li>
+                    <li><a class="nav-link scrollto" href="#team">Tentang Kami</a></li>
+                    <li><a class="nav-link scrollto" href="#why-us">Pertanyaan</a></li>
+                    <li><a class="getstarted scrollto" href="#about">Masuk</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav><!-- .navbar -->
+
+        </div>
+    </header><!-- End Header -->
+    <main id="main">
+        <section id="team" class="team section-bg">
+            <div class="container" data-aos="fade-up">
+                <div class="section-title">
+                    <h2>UP2D LAMPUNG</h2>
+                    <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+                </div>
+
+    </main>
+
+    <!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-5 col-md-6 footer-contact">
+                        <h3>PLN UP2D</h3>
+                        <p>
+                            JL. Z.A Pagar Alam No.05 <br>
+                            Bandar Lampung, 35144<br><br>
+                        </p>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 footer-links">
+                        <h4>Link Alternatif</h4>
+                        <ul>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Beranda</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Cari</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Tentang Kami</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Pertanyaan</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 footer-links">
+                        <h4>Kontak Kami</h4>
+                        <div class="social-links mt-3">
+                            <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                            <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                            <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                            <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+                        </div><br>
+                        <p><b>Telepon</b> : (0721) 773296</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="container footer-bottom clearfix">
+            <div class="copyright">
+                &copy; Copyright 2024 <strong><span>PLN UP2D LAMPUNG</span></strong>. All Rights Reserved
+            </div>
+            <div class="credits">
+                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+            </div>
+        </div>
+    </footer><!-- End Footer -->
+
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="<?= base_url("assets/vendor/aos/aos.js") ?>"></script>
+    <script src="<?= base_url("assets/vendor/bootstrap/js/bootstrap.bundle.min.js") ?>"></script>
+    <script src="<?= base_url("assets/vendor/glightbox/js/glightbox.min.js") ?>"></script>
+    <script src="<?= base_url("assets/vendor/isotope-layout/isotope.pkgd.min.js") ?>"></script>
+    <script src="<?= base_url("assets/vendor/swiper/swiper-bundle.min.js") ?>"></script>
+    <script src="<?= base_url("assets/vendor/waypoints/noframework.waypoints.js") ?>"></script>
+    <script src="<?= base_url("assets/vendor/php-email-form/validate.js") ?>"></script>
+
+    <!-- Template Main JS File -->
+    <script src="<?= base_url("assets/js/main.js") ?>"></script>
+    <script src="<?= base_url("assets/js/app.js") ?>"></script>
+    <script src="<?= base_url("https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js") ?>"></script>
+
+</body>
+
+</html>
